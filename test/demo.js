6e66ae0af3673f5ab4a7ea5013c596b9e1275a05
Utkarsh Maheshwari <utkmaheshwari245@Utkarshs-MacBook-Air.local>
2019-10-07 12:37:54 +0530

    alice-bob

--- test/demo.js
+++ test/demo.js
@@ -8,37 +8,40 @@ const {proofs : {MINT_PROOF}} = utils;
 const {JoinSplitProof, MintProof} = aztec;
 
 contract('Test Aztec Privacy Protocol', accounts => {
+    const alice = secp256k1.generateAccount();
     const bob = secp256k1.generateAccount();
-    const sally = secp256k1.generateAccount();
 
     it('Simulate Minting and Unilateral Transfer', async() => {
         const ZkAssetMintableInstance = await ZkAssetMintable.deployed();
   
-        console.log('Give Bob notes worth 100');
-        const bobNote1 = await aztec.note.create(bob.publicKey, 100);
+        console.log('Give Alice notes worth 100');
+        const aliceNote1 = await aztec.note.create(alice.publicKey, 100);
+        console.log(aliceNote1);
         const currentTotalValueNote = await aztec.note.createZeroValueNote();
-        const newTotalValueNote = bobNote1;
-        const mintedNotes = [bobNote1];
+        const newTotalValueNote = aliceNote1;
+        const mintedNotes = [aliceNote1];
         const senderForMinting = ZkAssetMintableInstance.address;
         const mintProof = new MintProof(currentTotalValueNote, newTotalValueNote, mintedNotes, senderForMinting);
         const mintData = mintProof.encodeABI();
         const mint = await ZkAssetMintableInstance.confidentialMint(MINT_PROOF, mintData);
-        console.log(mint.logs);
-        console.log('Successfully given notes worth 100 to Bob');
+        console.log(senderForMinting);
+        console.log(alice.address);
+        console.log(mint);
+        console.log('Successfully given notes worth 100 to Alice');
 
-        console.log('Transfer notes worth 25 from Bob to Sally');
-        const bobNote2 = await aztec.note.create(bob.publicKey, 75);
-        const sallyNote1 = await aztec.note.create(sally.publicKey, 25);
-        const inputNotes = [bobNote1];
-        const outputNotes = [bobNote2, sallyNote1];
+        console.log('Transfer notes worth 25 from Alice to Bob');
+        const aliceNote2 = await aztec.note.create(alice.publicKey, 75);
+        const bobNote1 = await aztec.note.create(bob.publicKey, 25);
+        const inputNotes = [aliceNote1];
+        const outputNotes = [aliceNote2, bobNote1];
         const senderForTransfer = accounts[0];
         const publicValue = 0;
-        const publicOwner = bob.address;
+        const publicOwner = alice.address;
         const sendProof = new JoinSplitProof(inputNotes, outputNotes, senderForTransfer, publicValue, publicOwner);
         const sendProofData = sendProof.encodeABI(ZkAssetMintableInstance.address);
-        const sendProofSignatures = sendProof.constructSignatures(ZkAssetMintableInstance.address, [bob]);
+        const sendProofSignatures = sendProof.constructSignatures(ZkAssetMintableInstance.address, [alice]);
         const transfer = await ZkAssetMintableInstance.confidentialTransfer(sendProofData, sendProofSignatures);
         console.log(transfer.logs);
-        console.log('Successfully transferred notes worth 25 from Bob to Sally');
+        console.log('Successfully transferred notes worth 25 from Alice to Bob');
     });
 });
