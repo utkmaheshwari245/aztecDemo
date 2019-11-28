913f2bb6c1148cfd3f865e8bd7cc8539d0f11d39
Your Name <utkmaheshwari245@gmail.com>
2019-11-28 16:16:13 +0530

    update setup

--- truffle-config.js
+++ truffle-config.js
@@ -1,18 +1,6 @@
 require('@babel/register');
 
 module.exports = {
-  compilers: {
-    solc: {
-      version: '0.5.4',
-      settings: {
-        optimizer: {
-          enabled: true,
-          runs: 500,
-        },
-        evmVersion: 'constantinople',
-      },
-    },
-  },
   networks: {
     development: {
       host: '127.0.0.1',
